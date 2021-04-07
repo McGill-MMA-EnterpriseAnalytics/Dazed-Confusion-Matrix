9742d7a1bb6e59fd427836ec738d21177d017dc8
Bogdan Tanasie <tanasie.bogdan21@gmail.com>
2021-04-07 15:20:51 -0400

    Working causal models

--- Causal/CausalModel.py
+++ Causal/CausalModel.py
@@ -5,38 +5,48 @@ from causallift import CausalLift
 from IPython.display import display
 
 
-def causal_lift_model(cm_df, treatment_name, seed=5):
+def causal_lift_model(cm_df, decoder, treatment_name, use_multi=True, seed=5):
     # Need to drop null for computations - could also impute these
-    cm_df['treatment_name'] = cm_df['treatment_name'].dropna()
-    cm_df.reset_index(inplace=True)
+    original_len = len(cm_df)
+    print('Original data length {}'.format(original_len))
+    cm_df[treatment_name] = cm_df[treatment_name].dropna().reset_index(drop=True)
+    clean_len = len(cm_df)
+    print('Clean data length {}'.format(clean_len))
+    ratio_dropped = (original_len - clean_len) / clean_len
 
     # Separate treatment into two categories (below or above average)
-    treatment_below_avg = cm_df['treatment_name'].mean()
-    cm_df['Treatment'] = np.where(cm_df['treatment_name'] <= treatment_below_avg, 1, 0)
+    treatment_below_avg = cm_df[treatment_name].mean()
+    cm_df['Treatment'] = np.where(cm_df[treatment_name] <= treatment_below_avg, 1, 0)
 
-    # Get each outcome and loop through them testing them as binary variables
-    outcome_names = set(cm_df['Description'].values)
-
-    for outcome in outcome_names:
-        print('\n{}'.format(outcome))
-        cm_df['Outcome'] = np.where(cm_df['Description'] == outcome, 1, 0)
+    # Use multiple classes as target
+    if use_multi:
+        cm_df['Outcome'] = cm_df['Description']
+        # Use the top 10 features
         cm_df = cm_df[['Treatment', 'Outcome',
-                       'Weapon_FIREARM', 'Weapon_HANDS', 'Weapon_KNIFE', 'Weapon_NONE', 'Weapon_OTHER',
-                       'Season_autumn', 'Season_spring', 'Season_summer', 'Season_winter',
-                       'Holiday', 'Weekend', 'Neighborhood', 'Premise', 'Month', 'Hour', 'Year', 'Outside']]
-
-        train_df, test_df = train_test_split(cm_df, test_size=0.3, random_state=seed, stratify=cm_df['Treatment'])
+                            'Weapon_FIREARM', 'Weapon_HANDS', 'Weapon_KNIFE', 'Weapon_NONE', 'Weapon_OTHER',
+                            'Neighborhood', 'Premise', 'Month', 'Hour', 'Outside']]
+        train_df, test_df = train_test_split(cm_df, test_size=0.2, random_state=seed, stratify=cm_df['Treatment'])
 
         print('\n[Estimate propensity scores for Inverse Probability Weighting.]')
-        cl = CausalLift(train_df, test_df, enable_ipw=True, verbose=3)
+        CausalLift(train_df, test_df, enable_ipw=True, verbose=3)
 
-        print(
-            '\n[Create 2 models for treatment and untreatment and estimate CATE (Conditional Average Treatment Effects)]')
-        train_df, test_df = cl.estimate_cate_by_2_models()
+    # Use each class as a binary outcome
+    else:
+        # Get each outcome and loop through them testing them as binary variables
+        outcome_names = set(cm_df['Description'].values)
+        print(outcome_names)
 
-        print('\n[Estimate the effect of recommendation based on the uplift model]')
-        estimated_effect_df = cl.estimate_recommendation_impact()
+        for outcome in outcome_names:
+            cm_df_copy = cm_df.copy()
+            print('\n{}'.format(decoder[outcome]))
+            cm_df_copy['Outcome'] = np.where(cm_df_copy['Description'] == outcome, 1, 0)
+            print(cm_df_copy['Outcome'])
+            # Use the top 10 features
+            cm_df_copy = cm_df_copy[['Treatment', 'Outcome',
+                           'Weapon_FIREARM', 'Weapon_HANDS', 'Weapon_KNIFE', 'Weapon_NONE', 'Weapon_OTHER',
+                           'Neighborhood', 'Premise', 'Month', 'Hour', 'Outside']]
 
-        print('\n[Show the estimated effect of recommendation based on the uplift model]')
-        display(estimated_effect_df)
+            train_df, test_df = train_test_split(cm_df_copy, test_size=0.2, random_state=seed, stratify=cm_df_copy['Treatment'])
 
+            print('\n[Estimate propensity scores for Inverse Probability Weighting.]')
+            CausalLift(train_df, test_df, enable_ipw=True, verbose=3)
